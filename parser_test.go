ee6231e09dd07e9564dfe324b23fad67cbe1f750
Chris Sloey <chris@addjam.com>
2019-11-11 12:38:17 +0000

    Add parser test

---
+++ parser_test.go
@@ -0,0 +1,29 @@
+package main
+
+import "testing"
+
+func TestExtractPeopleWithConsent(t *testing.T) {
+	store := PeopleStore{
+		people: []Person{},
+	}
+
+	inputData := InputData{
+		rolloverMode:       false,
+		benefitAmountPence: 61000, // £610
+
+		benefitExtractPath: "./testdata/Benefit Extract_06_09_19.txt",
+		dependentsSHBEPath: "./testdata/dependants SHBE_06-09-19-2.xlsx",
+		hbucdPath:          "./testdata/hb-uc.d-06-09-19.txt",
+		fsmCgAwardsPath:    "./testdata/FSM&CGawards_06-09-19.xlsx",
+		schoolRollPath:     "./testdata/School Roll Pupil Data_06-09-19-2.xlsx",
+		consent360Path:     "./testdata/Consent Report W360.xls",
+	}
+
+	t.Run("finds the correct matches", func(t *testing.T) {
+		ExtractPeopleWithConsent(inputData, &store)
+
+		if len(store.people) != 3 {
+			t.Errorf("Expected 3 people in store, got %d", len(store.people))
+		}
+	})
+}
