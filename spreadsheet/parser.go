91c45edb2fae4d8facb9b25c9e89ba907f09811d
Chris Sloey <chris@addjam.com>
2019-11-13 14:49:05 +0000

    Add NewParser(path) that creates the appropraite parser

--- spreadsheet/parser.go
+++ spreadsheet/parser.go
@@ -3,6 +3,8 @@ package spreadsheet
 import (
 	"errors"
 	"io"
+	"log"
+	"path/filepath"
 )
 
 var (
@@ -22,4 +24,28 @@ type Parser interface {
 // Row refers to a row in a spreadsheet, which has many columns
 type Row interface {
 	Col(int) string
+}
+
+// NewParser creates a parser appropriate for the spreadsheet at the given path.
+// Supports:
+//   - CSV
+//   - xls
+//   - xlsx
+func NewParser(path string) Parser {
+	extension := filepath.Ext(path)
+
+	switch extension {
+	case ".xls":
+		return NewXlsParser(path)
+	case ".xlsx":
+		return NewXlsxParser(path)
+	case ".txt":
+		return NewCsvParser(path)
+	case ".csv":
+		return NewCsvParser(path)
+	}
+
+	log.Fatalf("No parser for extension %s of file at path %s\n", extension, path)
+
+	return nil
 }
